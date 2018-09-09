1cf6495f0eb33e53732e325102306e53b762cac3
Andrew <Mclellan.an@gmail.com>
2018-09-09 12:10:41 -0400

    migrate

--- db/migrate/20180827232912_devise_create_users.rb
+++ db/migrate/20180827232912_devise_create_users.rb
@@ -6,6 +6,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :location
+      t.string :mlb_team
+      t.string :nlf_team
+      t.string :nba_team
+      t.string :nhl_team
+
 
       ## Recoverable
       t.string   :reset_password_token
